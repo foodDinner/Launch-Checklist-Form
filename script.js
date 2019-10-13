a92072ed1134ca3eec6377dd03e6f35f78bca47c
foodDinner <specialdelivery@gmail.com>
2019-10-12 19:58:06 -0500

    Added mission fetch with json

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load",function() {
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+         const missionData = document.getElementById("missionTarget");
+         missionData.innerHTML =`
+            <h2>Mission Destination</h2>
+            <ol>
+            <li>Name: ${json[0].name}</li>
+            <li>Diameter: ${json[0].diameter}</li>
+            <li>Star: ${json[0].star}</li>
+            <li>Distance from Earth: ${json[0].distance}</li>
+            <li>Number of Moon: ${json[0].moons}</li>
+            </ol>
+            <img src="${json[0].image}"></img>
+        `;
+      })
+   })
    let form = document.querySelector("form");
    
    form.addEventListener("submit",function() {
